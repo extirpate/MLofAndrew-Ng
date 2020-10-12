5791252ee421513b482c8a42e0800783dff502c3
extirpate <extirpate0832@gmail.com>
2020-10-13 00:58:19 +0800

    改成class的模式

--- source/guard2.py
+++ source/guard2.py
@@ -10,19 +10,32 @@ class Calc(object):
     @property
     def factory_base(self):
         #获得工厂参数基础数据
-        df=pd.read_excel('../其他文件/basedata.xlsx',skiprows=1,na_values=0,nrows=20,index_col='lev',na_filter=False)
+        df=pd.read_excel('../其他文件/basedata.xlsx',skiprows=1,na_values=0,nrows=20,index_col='lev',na_filter=True
+                         ,usecols=['lev','cost','get','hourP','storage'])
         df['cost']=df['cost'].astype(np.float64)
         df['get']=df['get'].astype(np.float64)
+        total_cost_iron=self.__calc_fac_needs(df)
         df=df.loc[list(self.base_data['factories']),:]
         self.base_hour_p=df['hourP'].sum() #每小时4个工厂提供的基础加工量
         self.per_slot_storage=df['storage'].sum() #4个加工厂提供的材料投放量,每个槽的投放量
-        return {'fac_base':self.base_hour_p,'fac_store':self.per_slot_storage}
+        return {'fac_base':self.base_hour_p,'fac_store':self.per_slot_storage,'total_cost_iron':total_cost_iron}
+
+    def __calc_fac_needs(self,df:pd.DataFrame):
+        total_cost=0
+        for i in list(self.base_data['factories']):
+            this_one_need= list(df.loc[df.index>=i,['cost']].sum())[0]
+            total_cost+=this_one_need
+        return total_cost
+
+
+
+
 
     #根据参数表中的自定义参数，获取基本配置信息
     #加成后的每小时加工数量(produce),加成后的每个槽位存储量(storage),满负荷加工时间(hours,mins),通道数(slots)
     @property
     def base_data(self):
-        df=pd.read_excel('../其他文件/basedata.xlsx',skiprows=49,index_col='index',nrows=11,usecols=['index','datas'])
+        df=pd.read_excel('../其他文件/basedata.xlsx',skiprows=49,index_col='index',nrows=15,usecols=['index','datas'])
         df=df.dropna()
         def got(a):
             b=eval(a)
@@ -42,13 +55,14 @@ class Calc(object):
                 'target_resist':df.loc[['target_resist'],'datas'][0],
                 'spe_provide':df.loc[['spe_provide'],'datas'][0],
                 'having_posi':df.loc[['having_posi'],'datas'][0],
-                'grows':df.loc[['grows'],'datas'][0],
+                'having_iron':df.loc[['having_iron'],'datas'][0],
+                'grows_posi':df.loc[['grows_posi'],'datas'][0],
+                'grows_iron':df.loc[['grows_iron'],'datas'][0],
+                'iron_dis':df.loc[['iron_dis'],'datas'][0],
                 }
         return self.result
     @property
     def effective(self):
-        # per_slot_storage=self.factory()[1]
-        # produce = base_hour_p*(1+fac_rate)*1.405 #经验观察值，而非面板写的50%提升，也不是18%+50%,所以这里没有使用buy_rate
         produce = self.factory_base['fac_base']*(1+self.base_data['fac_rate']+self.base_data['buy_rate'])/1.013333333
         #经验观察值，而非面板写的50%提升，也不是18%+50%,
         storage = self.factory_base['fac_store']+self.base_data['buy_store']+self.base_data['fac_store']
@@ -56,20 +70,6 @@ class Calc(object):
         mins = round((storage/produce-hours)*60,2)
         #加成后每个通道每小时的加工数量，  每个通道的储量,  满负荷下可以加工多少小时，分钟
         return {'produce':round(produce,2), 'storage':storage, 'hours':hours,'mins':mins}
-
-    def fac_cal(self,need_p:int):
-        produces=countFactory()
-        slots=produces[4]
-        hourP=round(produces[0]*slots,2)
-        print('单通道每小时加工{},  {}个槽合计每小时加工:{}'.format(round(produces[0],2),slots,hourP))
-        grows=self.base_data['grows']
-        print('每小时可以产出{}'.format(grows))
-        hours=round(need_p/hourP,2)
-        days=round(hours/24,2)
-        print('加工需要:{}小时,折合{}天'.format(hours,days))
-        hours_grow=round(need_p/grows,2)
-        days_grow=round(hours_grow/24,2)
-        print('产出目标所需量{}小时,折合{}天'.format(hours_grow,days_grow))
 
 
     #根据参数文件，计算需要的病毒等级数(needs)
@@ -91,7 +91,7 @@ class Calc(object):
             needs=needs.append(df_ac)
         needs.sort_values(by='cost',inplace=True)
         needs.reset_index(inplace=True,drop=True)
-        needs=needs.iloc[0:need-1,:]
+        needs=needs.iloc[0:need,:]
         #返回dataframe， 第一列是ac等级,第二列是需要的数值
         return needs
 
@@ -119,9 +119,7 @@ class Calc(object):
         need=target-talent_prov-nowlev
         return need
 
-    def
-
-def __main__():
+if __name__ =='__main__':
     a = Calc()
     target=a.base_data['target_resist']
     levs=a.base_data['poisons']
@@ -133,25 +131,35 @@ def __main__():
     need_levs=result[0]
     posi_material=result[1]
     details=result[2]
-    print('到目标{}级，还需要：{}级，共计{}材料'.format(target,need_levs,posi_material))
+    print('-------------1.关于土地生产所需材料----------')
+    print('毒抗到目标{}级，还需要：{}级，共计{}材料'.format(target,need_levs,posi_material))
     print('需要的ac及等级明细，开销如下：')
     print(details)
     now_have_p=a.base_data['having_posi']
     need_p=posi_material-int(now_have_p)
-    print('一共需要{}病毒材料,扣除现有的{},还需要:{}材料'.format(posi_material,now_have_p,need_p))
-
-    produces=countFactory()
-    slots=produces[4]
-    hourP=round(produces[0]*slots,2)
-    print('单通道每小时加工{},  {}个槽合计每小时加工:{}'.format(round(produces[0],2),slots,hourP))
-    grows=1600*91*(1+0.29)
-    print('每小时可以产出{}'.format(grows))
-    hours=round(need_p/hourP,2)
-    days=round(hours/24,2)
-    print('加工需要:{}小时,折合{}天'.format(hours,days))
-    hours_grow=round(need_p/grows,2)
+    print('如上所示，一共需要{}病毒材料,扣除现有的{},还需要:{}材料'.format(posi_material,now_have_p,need_p))
+    grows_posi=a.base_data['grows_posi']
+    grows_iron=a.base_data['grows_iron']
+    print('目前每小时土地收获{}病毒材料,{}复合材料,每小时共收获材料{}'.format(grows_posi,grows_iron,grows_posi+grows_iron))
+    hours_grow=round(need_p/grows_posi,2)
     days_grow=round(hours_grow/24,2)
-    print('产出目标所需量{}小时,折合{}天'.format(hours_grow,days_grow))
+    print('树田产出目标所需量病毒材料要{}小时,折合{}天'.format(hours_grow,days_grow))
+
+    need_iron= a.factory_base['total_cost_iron']
+    hav_iron=a.base_data['having_iron']
+    real_need_iron=round((need_iron-hav_iron)*(1-a.base_data['iron_dis']))
+    hours_iron=round(real_need_iron/grows_iron)
+    days_iron=round(hours_iron/24,2)
+    print('4工厂升至20级，尚需{}复合材料,按每小时土地收获{}油铁，需要{}小时,折合{}天'.format(real_need_iron,a.base_data['grows_iron'],hours_iron,days_iron))
 
 
-__main__()
+    print('-----------2.关于加工能力-----------')
+    produces=a.effective['produce']
+    slots=a.base_data['slots']
+    hourP=round(produces*slots,2)
+    print('单通道每小时加工{},  {}个槽合计每小时加工:{}'.format(produces,slots,hourP))
+
+    hours_p=round((need_p+real_need_iron)/hourP,2)
+    days_p=round(hours_p/24,2)
+    print('加工复合材料及病毒材料到目标等级需要:{}小时,折合{}天'.format(hours_p,days_p))
+
